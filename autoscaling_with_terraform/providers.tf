e89a5fa5d8477709e0f48ad15fce7ad9b013610b
Noble Mulaudzi <110469709+Mutuwa99@users.noreply.github.com>
2023-01-11 21:25:26 +0200

    rm pr

--- autoscaling_with_terraform/providers.tf
+++ autoscaling_with_terraform/providers.tf
@@ -1,15 +1,2 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
 
-provider "aws" {
-  region     = "af-south-1"
-  access_key = "AKIA436U47JYPYVZLJGV"
-  secret_key = "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"
-  profile    = "Noble"
-}
 
